98d07d0d695230531ae6708d644c4c39d6d646be
Neal <neal@waiata.net>
2023-02-14 15:23:47 +0800

    commented outTaptics in Buzzer

--- Stroop/Views/Buzzer.swift
+++ Stroop/Views/Buzzer.swift
@@ -10,24 +10,24 @@ import SwiftUI
 class Buzzer {
     
     
-    typealias Taptics = UINotificationFeedbackGenerator
-    typealias Feedback = Taptics.FeedbackType
-    
+//    typealias Taptics = UINotificationFeedbackGenerator
+//    typealias Feedback = Taptics.FeedbackType
+//
     static let shared = Buzzer()
     
-    let taptic = Taptics()
+//    let taptic = Taptics()
     
     init() {
-        taptic.prepare()
+//        taptic.prepare()
     }
     
-    func feedback(_ feedback: Feedback = .success) {
-        taptic.notificationOccurred(feedback)
-    }
+//    func feedback(_ feedback: Feedback = .success) {
+//        taptic.notificationOccurred(feedback)
+//    }
     
     func buzz(_ good: Bool? = true) {
         guard let good else { return }
-        let feedback: Feedback = good ? .success : .error
-        self.feedback(feedback)
+//        let feedback: Feedback = good ? .success : .error
+//        self.feedback(feedback)
     }
 }
