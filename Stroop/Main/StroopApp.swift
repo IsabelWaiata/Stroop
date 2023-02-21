a6434087c404227f97461a56621b3b2a094c5440
Neal <neal@waiata.net>
2023-02-22 03:39:35 +0800

    trying ReferenceFileDocument

--- Stroop/Main/StroopApp.swift
+++ Stroop/Main/StroopApp.swift
@@ -11,10 +11,16 @@ import SwiftUI
 struct StroopApp: App {
     
     var body: some Scene {
-        DocumentGroup(newDocument: StroopDocument()) { file in
-            ContentView()
-                .environmentObject(file.document.stroop)
-                .frame(minWidth: 120, maxWidth: .infinity, minHeight: 80, maxHeight: .infinity)
-        }
+        DocumentGroup(newDocument: {
+                StroopDocument()
+            }) { file in
+                ContentView(doc: file.document)
+            }
+        
+//        DocumentGroup(newDocument: StroopDocument()) { file in
+//            ContentView(doc: file.$document)
+////                .environmentObject(file.document.stroop)
+//                .frame(minWidth: 120, maxWidth: .infinity, minHeight: 80, maxHeight: .infinity)
+//        }
     }
 }
--- Stroop/Main/StroopDocument.swift
+++ Stroop/Main/StroopDocument.swift
@@ -9,24 +9,19 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 
-struct StroopDocument: FileDocument {
+class StroopDocument: ReferenceFileDocument {
+   
+    @Published var stroop: Stroop
     
-    var stroop: Stroop
-
     init() {
         stroop = Stroop()
     }
     
-    init(configuration: ReadConfiguration) throws {
-        guard let data = configuration.file.regularFileContents,
-              let stroop = try? JSONDecoder().decode(Stroop.self, from: data)
-        else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-        self.stroop = stroop
+    func snapshot(contentType: UTType) throws -> Stroop {
+        return self.stroop
     }
     
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+    func fileWrapper(snapshot: Stroop, configuration: WriteConfiguration) throws -> FileWrapper {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
         do {
@@ -36,6 +31,27 @@ struct StroopDocument: FileDocument {
             throw error
         }
     }
+
+    
+    required init(configuration: ReadConfiguration) throws {
+        guard let data = configuration.file.regularFileContents,
+              let stroop = try? JSONDecoder().decode(Stroop.self, from: data)
+        else {
+            throw CocoaError(.fileReadCorruptFile)
+        }
+        self.stroop = stroop
+    }
+//
+//    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+//        let encoder = JSONEncoder()
+//        encoder.outputFormatting = .prettyPrinted
+//        do {
+//            let data = try encoder.encode(stroop)
+//            return .init(regularFileWithContents: data)
+//        } catch {
+//            throw error
+//        }
+//    }
 
     static var readableContentTypes: [UTType] { [.stroop] }
 }
